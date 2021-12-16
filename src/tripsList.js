ceb7969577028d7c01f6da30898205af2a8264ba
lbernasl <miguelbernardinoferreira@gmail.com>
2021-12-16 18:34:35 +0000

    tripinfo mid work

--- src/tripsList.js
+++ src/tripsList.js
@@ -6,31 +6,42 @@ import ListSubheader from '@mui/material/ListSubheader';
 import { addTrip } from './redux';
 import { connect } from 'react-redux'
 
-class TripsList extends React.Component{
-    render(){
-        return(
-        <ImageList sx={{ width: '92%', height: 'auto', marginLeft: '4%' }} gap={16}>
-            <ImageListItem key="Subheader" cols={2}>
-                <ListSubheader id="titles" component="div">Viagens sugeridas</ListSubheader>
-            </ImageListItem>
-            {this.props.trips.map((item) => (
-            <ImageListItem id="trip_card" key={item.img} sx={{ width: '100%'}}>
-                <img
-                src={`${process.env.PUBLIC_URL}${item.img}?w=248&fit=crop&auto=format`}
-                srcSet={`${process.env.PUBLIC_URL}${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
-                alt={item.title}
-                loading="lazy"
-                />
-                <ImageListItemBar
-                title={item.title}
-                actionIcon={item.date}
-                subtitle={item.description}
-                sx={{ backgroundColor: '#FFFFFF', paddingTop: '4px', boxShadow: '2px 2px 4px rgba(0, 0, 0, 0.05)', borderRadius: '0px 0px 10px 10px'}}
-                position="below"
-                />
-            </ImageListItem>
-            ))}
-        </ImageList>   
+class TripsList extends React.Component {
+    constructor(props){
+        super(props)
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick(index){
+       alert(index)
+    }
+
+    render() {
+        return (
+            <ImageList sx={{ width: '92%', height: 'auto', marginLeft: '4%' }} gap={16}>
+                <ImageListItem key="Subheader" cols={2}>
+                    <ListSubheader id="titles" component="div">Viagens sugeridas</ListSubheader>
+                </ImageListItem>
+                {this.props.trips.map((item, index) => (
+                    <button onClick={this.handleClick(index)}>
+                        <ImageListItem id="trip_card" key={item.img} sx={{ width: '100%' }}>
+                            <img
+                                src={`${process.env.PUBLIC_URL}${item.img}?w=248&fit=crop&auto=format`}
+                                srcSet={`${process.env.PUBLIC_URL}${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
+                                alt={item.title}
+                                loading="lazy"
+                            />
+                            <ImageListItemBar
+                                title={item.title}
+                                actionIcon={item.date}
+                                subtitle={item.description}
+                                sx={{ backgroundColor: '#FFFFFF', paddingTop: '4px', boxShadow: '2px 2px 4px rgba(0, 0, 0, 0.05)', borderRadius: '0px 0px 10px 10px' }}
+                                position="below"
+                            />
+                        </ImageListItem>
+                    </button>
+                ))}
+            </ImageList>
         );
     }
 }
@@ -38,13 +49,13 @@ class TripsList extends React.Component{
 let tripToAdd = {}
 
 const mapStateToProps = state => {
-    return{
+    return {
         trips: state.trips
     }
 }
 
 const mapDispatchToProps = dispatch => {
-    return{
+    return {
         addTrip: () => dispatch(addTrip(tripToAdd))
     }
 }
